133760648a7073142a7b90ccdf596de6dca04363
AdrianMvpDev <adrianimpor1@gmail.com>
2023-11-02 23:45:10 -0500

    configuración inicial

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import AppNavigator from './src/navigation/AppNavigator';
+import { Platform, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1, paddingTop: Platform.OS === 'android' ? 25 : 0 }}>
+      <AppNavigator />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,46 @@
+import { Text } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+export default function AppNavigator() {
+  const Tab = createBottomTabNavigator();
+
+  function MyTabs() {
+    return (
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+          showIcon: true,
+          tabBarLabel: () => null,
+          tabBarStyle: {
+            height: 60,
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={MyStack}
+          options={{
+            tabBarIcon: ({ focused, color }) => <Ionicons name="home" size={24} color={focused ? '#081f46' : '#b1abb8'} />,
+          }}
+        />
+
+        <Tab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{
+            tabBarIcon: ({ focused, color }) => <Ionicons name="search" size={24} color={focused ? '#081f46' : '#b1abb8'} />,
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
